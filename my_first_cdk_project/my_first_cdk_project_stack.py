c2be8c06237a863023dc4b3448d73982fab4bc4a
sanoaoa <ranju.koar@gmail.com>
2021-06-18 15:46:32 +0530

    adding my first changes

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,15 +1,25 @@
-from aws_cdk import core as cdk
-
+from aws_cdk import (
+    aws_s3 as _s3,
+    core
+    
+)
 # For consistency with other languages, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
 # with examples from the CDK Developer's Guide, which are in the process of
 # being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+# from aws_cdk import core
 
 
-class MyFirstCdkProjectStack(cdk.Stack):
+class MyFirstCdkProjectStack(core.Stack):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        _s3.Bucket(
+            self,
+            "myBucketId",
+            bucket_name="myfirstcdkbucker1234567uiapidb",
+            versioned=True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+        )
